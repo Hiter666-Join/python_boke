1e039ed21e419369ae65b1ea9aeb4450a44405e6
Hiter John <3557305035@qq.com>
2026-01-02 00:15:51 +0800

    main all 12-01

--- static/js/switch.js
+++ static/js/switch.js
@@ -1,7 +1,7 @@
+// 切换照片
 document.getElementById('theme-toggle').addEventListener('click',()=>{
   const html=document.documentElement,
         now=html.getAttribute('data-theme'),
         next=now==='dark'?'light':'dark';
   html.setAttribute('data-theme',next);
-  localStorage.setItem('theme',next);
 });
